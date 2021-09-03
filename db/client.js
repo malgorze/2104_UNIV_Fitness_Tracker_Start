d289e7c1bfea38596eab5981d6fea1125e8f4a86
malgorze <gosia.popowska@gmail.com>
2021-09-02 19:58:14 -0500

    seedDate update

--- db/client.js
+++ db/client.js
@@ -1,2 +1,6 @@
 // build and export your unconnected client here
+require("dotenv").config();
+const { Client } = require("pg");
+const client = new Client("postgres://localhost:5432/fitness-dev");
 
+module.exports = { client };

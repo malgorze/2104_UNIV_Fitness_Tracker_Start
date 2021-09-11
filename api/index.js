bebb0c9e323f1738b8c86375df0f21dec956a39a
KyleThie <kylethie@gmail.com>
2021-09-11 13:04:16 -0500

    added dependencies

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 // create an api router
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+ console.log('Server is up and running on port ', port);
+})
+
+
 // attach other routers from files in this api directory (users, activities...)
+
+const usersRouter = require("./users");
+const activitiesRouter = require("./activities");
+const routinesRouter = require("./routines");
+
+
+
 // export the api router
 // test comment

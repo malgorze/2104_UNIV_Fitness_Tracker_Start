9b395fa1ccabc0bd9f3edff85357df519ff33ccb
malgorze <gosia.popowska@gmail.com>
2021-09-14 19:54:23 -0500

    err

--- api/activitiesR.js
+++ api/activitiesR.js
@@ -4,28 +4,31 @@ const activitiesRouter = express.Router();
 // Just return a list of all activities in the database
 const { createActivity } = require("../db/activities");
 const { getAllActivities } = require("../db/activity");
-  activitiesRouter.get("/api/activities", async (req, res) => {
-    try {
-      const allActivities = await getAllActivities();
-      const activites = allActivities.filter((activities) => {
-        return activities;
-      });
-      res.send({
-        activites,
-      });
-    } catch (error)
+activitiesRouter.get("/api/activities", async (req, res) => {
+  try {
+    const allActivities = await getAllActivities();
+    const activites = allActivities.filter((activities) => {
+      return activities;
+    });
+    res.send({
+      activites,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 // POST /activities (*)
 // Create a new activity
 activitiesRouter.post("/api/activities", async (req, res) => {
-    try {
-        const newActivity = await createActivity();
-        if (activity) {
-            return activites
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      });
+  try {
+    const newActivity = await createActivity();
+    if (activity) {
+      return activites;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 // PATCH /activities/:activityId (*)
 // Anyone can update an activity (yes, this could lead to long term problems a la wikipedia)
 // GET /activities/:activityId/routines
